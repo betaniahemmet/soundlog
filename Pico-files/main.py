6370adfa992206e6cc8de9db931aba1b679e8399
henrik björserud <henrikbjorserud@gmail.com>
2023-04-28 12:10:09 +0200

    added functions to convert ptp amplitude to a value between 1 and 10

--- Pico-files/main.py
+++ Pico-files/main.py
@@ -10,7 +10,7 @@ from time import ticks_ms, ticks_add, ticks_diff
 ADC_PIN = Pin(26, mode=Pin.IN) # The microphone AUD is connected to pin 26 on the pico
 MIC = ADC(ADC_PIN) # The pin on which to read from the ADC
 VREF = 3.3 # The supply voltage, which affects for the readings from the ADC
-RANGES = 1 << 16 # Because of the 16-bit ADC of the pico. This is equal to 65536
+RANGES = 1 << 16 # Because of the 12-bit ADC of the pico being upsampled to 16 in micropython. This is equal to 65536
 MAX_RES = 65536
 CONV = VREF / RANGES # The resolution of the ADC is roughly 0.8 millivolts
 
@@ -18,7 +18,7 @@ CONV = VREF / RANGES # The resolution of the ADC is roughly 0.8 millivolts
 samples = []
 
 def ptp():
-    """A function to find the peak to peak amplitude of the raw signal and convert it to volts"""
+    """A function to find the peak to peak amplitude of the raw signal"""
     max_amp = 0
     min_amp = 65536
     adc_raw = MIC.read_u16()
@@ -32,11 +32,40 @@ def ptp():
                 min_amp = adc_raw
                 
     ptp_amp = max_amp - min_amp
-    adc_raw_volts = (ptp_amp * VREF) / MAX_RES
-    return round(adc_raw_volts, 2)
+    
+    return ptp_amp
 
-print(ptp())
 
+def ptp_to_volts(ptp_amp):
+    """Turn the peak to peak amplitude into volts"""
+    
+    amplitude_as_volts = (ptp_amp * VREF) / MAX_RES
+    
+    return amplitude_as_volts
+
+def value_mapping(volt_reading):
+    """ Map the volts onto a scale from 1 to 10"""
+    
+    normalized_volts = volt_reading / VREF
+    mapped_value = normalized_volts * 10
+    rounded_value = round(mapped_value, 2) # Round value to only two decimals
+    
+    return rounded_value
+
+x = 0
+while x < 500:
+    ptp_amp = ptp()
+    volts = ptp_to_volts(ptp_amp)
+    final_value = value_mapping(volts)
+    print(final_value)
+    x += 1
+    
+
+#try:
+#    connect()
+#except KeyboardInterrupt:
+#    machine.reset()
+    
 def connect():
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
@@ -49,20 +78,3 @@ def connect():
         ip = wlan.ifconfig()[0]
     print(f'Connected on {ip}')
     return ip
-
-"""
-while True:
-    adc_raw = MIC.read_u16()
-    sample = round(adc_raw * CONV, 2)
-    samples.append(sample)
-    # sleep(0.001)
-    if len(samples) == 1000:
-        print(max(samples))
-        samples = []
-"""       
-#try:
-#    connect()
-#except KeyboardInterrupt:
-#    machine.reset()
-    
-
